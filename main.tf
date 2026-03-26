fb15661299d74f8c8f1367c2580272d93d25979e
unknown <Administrator@KSI-NVM-PHANI.KEYUSA.LOCAL>
2026-03-26 11:29:53 -0700

    re-uploading the content

---
+++ main.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "instance" {
+  for_each =  var.COMPONENTS
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = "sg-0ce01673ed9893e29"
+
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_route53_record" "route-setup" {
+  zone_id = "Z02549774QMYGMZM7W06"
+  name    = "${each.key}-dev"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.instance[each.key].private_ip"]
+}
+
+
+variable "COMPONENTS" {
+    default= {
+        frontend = ""
+        postgresql = ""
+        portfolio-service = ""
+        auth-service = ""   
+        analytics-service = ""
+    }
+}
